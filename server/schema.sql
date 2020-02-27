bfa94d210129917e718bf5176e45c7ae2f7e6ba6
kelsyvghn <angelstate@gmail.com>
2020-02-26 16:40:05 -0800

    implement schema.sql

--- server/schema.sql
+++ server/schema.sql
@@ -3,12 +3,29 @@ CREATE DATABASE chat;
 USE chat;
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  /* Entity for messages - holds a UUID, message text, room name, and user's UUID*/
+  id INT(100) NOT NULL AUTO_INCREMENT,
+  text varchar(280),
+  roomname char(255),
+  userid INT(10),
+  Primary KEY (id)
 );
 
 /* Create other tables and define schemas for them here! */
+CREATE TABLE users (
+  /* Entity for users - holds a UUID, username */
+  id INT(100) NOT NULL AUTO_INCREMENT,
+  userid INT(10),
+  username varchar(15),
+  Primary KEY (id)
+);
 
-
+CREATE TABLE rooms (
+  /* Entity for messages - holds a UUID, message text, room name, and user's UUID*/
+  id INT(100) NOT NULL AUTO_INCREMENT,
+  roomname char(255),
+  Primary KEY (id)
+);
 
 
 /*  Execute this file from the command line by typing:
