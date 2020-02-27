57ffa7ba6bb76136a13bf4970f006402c3ca3f95
kelsyvghn <angelstate@gmail.com>
2020-02-26 17:29:56 -0800

    made minor changes to spec. unable to create due to existing chat db

--- server/schema.sql
+++ server/schema.sql
@@ -4,28 +4,28 @@ USE chat;
 
 CREATE TABLE messages (
   /* Entity for messages - holds a UUID, message text, room name, and user's UUID*/
-  id INT(100) NOT NULL AUTO_INCREMENT,
+  id INT NOT NULL AUTO_INCREMENT,
   text varchar(280),
-  roomname char(255),
-  userid INT(10),
+  roomname char,
+  userid INT,
   Primary KEY (id)
 );
 
 /* Create other tables and define schemas for them here! */
-CREATE TABLE users (
-  /* Entity for users - holds a UUID, username */
-  id INT(100) NOT NULL AUTO_INCREMENT,
-  userid INT(10),
-  username varchar(15),
-  Primary KEY (id)
-);
+-- CREATE TABLE users (
+--   /* Entity for users - holds a UUID, username */
+--   id INT(100) NOT NULL AUTO_INCREMENT,
+--   userid INT(10),
+--   username varchar(15),
+--   Primary KEY (id)
+-- );
 
-CREATE TABLE rooms (
-  /* Entity for messages - holds a UUID, message text, room name, and user's UUID*/
-  id INT(100) NOT NULL AUTO_INCREMENT,
-  roomname char(255),
-  Primary KEY (id)
-);
+-- CREATE TABLE rooms (
+--   /* Entity for messages - holds a UUID, message text, room name, and user's UUID*/
+--   id INT(100) NOT NULL AUTO_INCREMENT,
+--   roomname char(255),
+--   Primary KEY (id)
+-- );
 
 
 /*  Execute this file from the command line by typing:
